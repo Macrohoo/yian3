b592ec7733ae82ed0a89390a38f3c2dfaf7239e3
yian <105950987@qq.com>
2022-07-04 00:53:08 +0800

    feat: modify .gitignore

--- es/components/index.js
+++
@@ -1,4 +0,0 @@
-export default function haha() {
-    console.log('xixix');
-}
-//# sourceMappingURL=index.js.map
--- es/index.js
+++
@@ -1,4 +0,0 @@
-import haha from './components/index';
-console.log('haha');
-haha();
-//# sourceMappingURL=index.js.map
